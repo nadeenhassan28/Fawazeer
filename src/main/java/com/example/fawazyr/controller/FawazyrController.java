7efb1ed7d7d25d9b770b5c74998d73fef6b73b04
yassinshaher1 <yassin.shaher2005@gmail.com>
2024-08-14 10:11:44 +0300

    Made all the controllers into 1 and commented the rest to be used if the new one didn't work

---
+++ src/main/java/com/example/fawazyr/controller/FawazyrController.java
@@ -0,0 +1,41 @@
+package com.example.fawazyr.controller;
+
+import com.example.fawazyr.data.*;
+import com.example.fawazyr.service.WinnerService;
+import com.example.fawazyr.service.GiftsService;
+import com.example.fawazyr.service.PrizeHistoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fawazyr")
+public class FawazyrController {
+
+    private final WinnerService winnerService;
+    private final GiftsService giftsService;
+    private final PrizeHistoryService prizeHistoryService;
+
+    public FawazyrController(WinnerService winnerService, GiftsService giftsService, PrizeHistoryService prizeHistoryService) {
+        this.winnerService = winnerService;
+        this.giftsService = giftsService;
+        this.prizeHistoryService = prizeHistoryService;
+    }
+
+    // All the return values are just to avoid errors still haven't started in the logics of the code
+    @GetMapping("/winners")
+    public List<Winner> getAllWinners() {
+         return winnerService.getAllWinners();
+    }
+
+    @GetMapping("/gifts")
+    public List<Gifts> getAllGifts() {
+         return giftsService.getAllGifts();
+    }
+
+    @GetMapping("/prizeHistories")
+    public List<PrizeHistory> getAllPrizeHistories() {
+         return prizeHistoryService.getAllPrizeHistories();
+    }
+
+}
--- src/main/java/com/example/fawazyr/controller/GiftsController.java
+++ src/main/java/com/example/fawazyr/controller/GiftsController.java
@@ -1,26 +1,26 @@
-package com.example.fawazyr.controller;
-
-import com.example.fawazyr.data.Gifts;
-import com.example.fawazyr.service.GiftsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/gifts")
-public class GiftsController {
-
-    private final GiftsService giftsService;
-
-    public GiftsController(GiftsService giftsService) {
-        this.giftsService = giftsService;
-    }
-
-    @GetMapping
-    public List<Gifts> getAllGifts() {
-         return giftsService.getAllGifts();// adjust this
-    }
-
-}
+//package com.example.fawazyr.controller;
+//
+//import com.example.fawazyr.data.Gifts;
+//import com.example.fawazyr.service.GiftsService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/gifts")
+//public class GiftsController {
+//
+//    private final GiftsService giftsService;
+//
+//    public GiftsController(GiftsService giftsService) {
+//        this.giftsService = giftsService;
+//    }
+//
+//    @GetMapping
+//    public List<Gifts> getAllGifts() {
+//         return giftsService.getAllGifts();// adjust this
+//    }
+//
+//}
